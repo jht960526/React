a37eac4113b42f4e55184135ad1384e9532f2640
HongTak <qoqo110966@gmail.com>
2025-01-20 02:51:47 +0900

    1.React 세팅 및 기본 파일 수정
    1.React 세팅 및 기본 파일 수정

---
+++ reactstudy/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    
+    <div className="App">
+      <header>
+        <h1><a href="/">WEB</a></h1>
+      </header>
+
+      <nav>
+      <ol>
+      <li><a href="/read/1">HTML</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+      </ol>
+
+      </nav>
+    <article>
+      <h2>Welcome</h2>
+      Hello, React
+    </article>
+    </div>
+  );
+}
+
+export default App;
