e506948833b777944efba18a9140fe3d35769f10
HongTak <qoqo110966@gmail.com>
2025-01-21 02:55:53 +0900

    1-4 Component
    1-4 Component

--- reactstudy/src/App.js
+++ reactstudy/src/App.js
@@ -1,26 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Header(){
+  return <header>
+    <h1><a href="/">React</a></h1>
+  </header>
+}
+
+function Nav(){
+  return <nav>
+    <ol>
+      <li><a href="/read/1">HTML</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+    </ol>
+  </nav>
+}
+
+function Article(){
+  return <article>
+    <h2>Welcome</h2>
+    Hello, React
+  </article>
+}
+
 function App() {
   return (
     
     <div className="App">
-      <header>
-        <h1><a href="/">WEB</a></h1>
-      </header>
+      <Header></Header>
+      <Header></Header>
+      <Header></Header>
 
-      <nav>
-      <ol>
-      <li><a href="/read/1">HTML</a></li>
-      <li><a href="/read/2">css</a></li>
-      <li><a href="/read/3">js</a></li>
-      </ol>
-
-      </nav>
-    <article>
-      <h2>Welcome</h2>
-      Hello, React
-    </article>
+      <Nav></Nav>
+    <Article></Article>
     </div>
   );
 }
