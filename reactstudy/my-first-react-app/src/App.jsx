40d5a94dcfd8b9540a2b9f28866dbfd4dfe71438
HongTak <qoqo110966@gmail.com>
2025-02-28 03:22:27 +0900

    컴퍼넌트 props 전달하기
    컴퍼넌트 props 전달하기

--- reactstudy/my-first-react-app/src/App.jsx
+++ reactstudy/my-first-react-app/src/App.jsx
@@ -3,38 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
 
-
-function HelloWorld() {
-  return (
-    <>
-    
-    <h1 className="button">Hello World</h1>
-      <h1>Hello World</h1>
-    </>
-      
-    
-    
-  );
-}
-
-function Message(){
-  const name = 'Alice';
-  return (
-    <input type="input" maxLength={5} style={{backgroundColor:"red"}}/>
-  );
-}
-
-
 function App() {
-
+const detinations = [
+  {
+    place: "파리",
+    destination: "에펠탑과 카페페가 있는 도시시",
+  },
+];
   return (
-    <>
-      <div>
-        <HelloWorld/>
-        <helloWorld/>
-          
-        </div>
-    </>
+    <div>
+      <Container>여행지역</Container>
+      <Destination place={"파리"} destination={'에펠탑과 카페가 있는도시'}/>
+    <Destination {...destination[0]}/>
+    <Destination {...destination[0]}/>
+    </div>
   );
 }
 
---
+++ reactstudy/my-first-react-app/src/Destination.jsx
@@ -0,0 +1,10 @@
+const Destination = () => {
+  return (
+    <div>
+      <h2>{place}</h2>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default Destination;
