83ae327a4c232c3cc81532fef68412c67cec1091
Jeong HongTaek <jht960526@naver.com>
2025-04-15 10:02:20 +0900

    ContextAPI
    데이터가 필요한 컴포넌트들에게 전달하기 위해서 데이터를 prop으로 내려주는 문제가 있음

--- reactstudy/second-react-app/src/App.jsx
+++ reactstudy/second-react-app/src/App.jsx
@@ -1,34 +1,30 @@
 import { useReducer, useState } from 'react'
 import './App.css'
 
-const initial = {count: 0};
-
-const reducer = (state,action) => {
-  switch (action.type){
-    case "INCREASE": {
-      return { count : state.count + 1};
-    }
-
-    case "DECREASE":{
-      return { count : state.count - 1};
-    }
-
-    default: {
-      throw new Error("invalid type: " + action.type);
-    }
-  }
-};
-
-
 function App() {
-  const [state,dispatch] = useReducer(reducer,initial);
+  const user = {
+    name: "chelsu",
+  };
+
+  return(
+    <div>
+      <Child user = {user}/>
+    </div>
+  );
+}
+
+function Parent({user}){
   return (
     <div>
-      <h1>count: {state.count}</h1>
-      <button onClick={() => dispatch({type: "INCREASE"})}>+</button>
-      <button onClick={() => dispatch({type: "DECREASE"})}>-</button>
+      <Child user={user}/>
     </div>
-  )
+  );
+}
+
+function Child({user}){
+  return (
+    <div>{user.name}</div>
+  );
 }
 
 
