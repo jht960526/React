f580c1945b801eaf256970a3dd7120fdd9d3aa5e
Jeong HongTaek <jht960526@naver.com>
2025-04-15 13:07:47 +0900

    uesRef2
    uesRef를 활용한 타이머 예제

--- reactstudy/second-react-app/src/App.jsx
+++ reactstudy/second-react-app/src/App.jsx
@@ -4,11 +4,25 @@ import { MyComponent, MyProvider } from './MyConponent';
 import { ThemeComponent, ThemeProvider } from './Theme';
 
 function App() {
-  const ref = useRef(0);
-  console.log(ref.current);
+  const [seconds, setSeconds] = useState(0);
+  const timeRef = useRef(null);
+
+  const handleStart = () => {
+    if(timeRef.current) return;
+    timeRef.current = setInterval(()=>{
+     setSeconds((pre)=>pre + 1)
+      },1000);
+  };
+  const handleEnd = () => {
+    clearInterval(timeRef.current);
+    timeRef.current = null;
+  };
+
   return(
     <div>
-      App
+      <h1>Timer: {seconds}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleEnd}>End</button>
     </div>
   );
 }
