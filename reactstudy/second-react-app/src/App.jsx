26364508b9b96d6d1e902d60444f8ffd8aae628d
Jeong HongTaek <jht960526@naver.com>
2025-04-15 11:21:02 +0900

    useRef1
    useRef 사용 예시

--- reactstudy/second-react-app/src/App.jsx
+++ reactstudy/second-react-app/src/App.jsx
@@ -1,14 +1,14 @@
-import { useReducer, useState } from 'react'
+import { useReducer, useRef, useState } from 'react'
 import './App.css'
 import { MyComponent, MyProvider } from './MyConponent';
 import { ThemeComponent, ThemeProvider } from './Theme';
 
 function App() {
+  const ref = useRef(0);
+  console.log(ref.current);
   return(
     <div>
-      <ThemeProvider>
-        <ThemeComponent/>
-      </ThemeProvider>
+      App
     </div>
   );
 }
