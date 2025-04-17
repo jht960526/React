76ecad56c7c72d30186b9663edf0fd2fb9a55a7e
Jeong HongTaek <66667772+jht960526@users.noreply.github.com>
2025-04-17 15:44:18 +0900

    useEffect1
    useEffect를 이용해서 setState 적용

--- reactstudy/second-react-app/src/App.jsx
+++ reactstudy/second-react-app/src/App.jsx
@@ -2,47 +2,38 @@ import { useReducer, useRef, useState } from 'react'
 import './App.css'
 import { MyComponent, MyProvider } from './MyConponent';
 import { ThemeComponent, ThemeProvider } from './Theme';
+import React, { useEffect } from 'react';
 
-function FocusInput(){
-  const inputRef = useRef(null);
 
-  const handleClick = () =>{
-    inputRef.current.focus();
-  };
-  return(
+function Basic(){
+  useEffect(()=>{
+    console.log("mount");
+  },[]);
+  return <div>Hello world</div>
+}
+
+function Update(){
+  const [state,setState] = useState(0);
+  useEffect(()=>{
+    console.log("update",state);
+  },[state]);
+
+  return (
     <div>
-      <input type="text" ref={inputRef}/>
-      <button onClick={handleClick}>Focus</button>
+      <h1>state: {state}</h1>
+      <button onClick={()=>setState((pre)=>pre+1)}>Click</button>
     </div>
   );
 }
 
 function App() {
-  const [seconds, setSeconds] = useState(0);
-  const timeRef = useRef(null);
-
-  const handleStart = () => {
-    if(timeRef.current) return;
-    timeRef.current = setInterval(()=>{
-     setSeconds((pre)=>pre + 1)
-      },1000);
-  };
-  const handleEnd = () => {
-    clearInterval(timeRef.current);
-    timeRef.current = null;
-  };
-
+  
   return(
     <div>
-      <h1>Timer: {seconds}</h1>
-      <button onClick={handleStart}>Start</button>
-      <button onClick={handleEnd}>End</button>
-
-      <FocusInput/>
-
+      <Basic/>
+      <Update/>
     </div>
   );
 }
 
-
-export default App
+export default App;
