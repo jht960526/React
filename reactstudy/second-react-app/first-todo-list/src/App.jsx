e07610245a85d92a21e77a0bf0a52003dc257b40
Jeong HongTaek <66667772+jht960526@users.noreply.github.com>
2025-04-21 17:31:31 +0900

    TodoList제작1
    TodoList제작1

--- reactstudy/second-react-app/first-todo-list/src/App.jsx
+++ reactstudy/second-react-app/first-todo-list/src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import LayOut from './Layout'
+import Title from './Title'
+import Controls from './Controls'
 
 function App() {
   return (
-    <div>Hello world
+    <div>
+      <LayOut>
+      <Title/>
+      <Controls/>
+        Hello world
+      </LayOut>
+      {/* 
+        <Title/>
+        <Controls/>
+        <TodoList/>
+      </LayOut> */}
     </div>
   )
 }
---
+++ reactstudy/second-react-app/first-todo-list/src/Layout.jsx
@@ -0,0 +1,7 @@
+import "./Layout.css";
+
+function LayOut({children}){
+    return <div className="layout">{children}</div>
+}
+
+export default LayOut;
